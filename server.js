a8107a5d8ac67f8da9ef5f6676df5d203f19235a
Bhanuka Pannipitiya <bhanukapannipitiya@admins-MacBook-Pro.local>
2024-08-18 22:03:48 +0530

    Added a middleware to check the token verification

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ app.get("/", (req, res) => {
     res.send("Hello World 1123");
 });
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 
 app.listen(PORT, () => {
