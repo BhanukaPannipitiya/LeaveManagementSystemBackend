8c37f049d7be26794c2f533af98c57ed0699b3d4
Bhanuka Pannipitiya <bhanukapannipitiya@admins-MacBook-Pro.local>
2024-08-18 18:18:17 +0530

    Change the folder structure and project setup

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send("Signup ROUTE");
+}
+
+export const login = async (req, res) => {
+    res.send("Login ROUTE");
+}
+
+export const logout = async (req, res) => {
+    res.send("Logout ROUTE");
+}
--- src/routes/SignUp.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const {signupUser} = require('../controller/SignUp');
-const router = express.Router();
-
-router.post('/register', signupUser);
-
-module.exports = router;
